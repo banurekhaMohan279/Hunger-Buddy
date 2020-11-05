8db6385f970044a4f0bc6c60d8ea192386882754
Banurekha <banurekha279@gmail.com>
2020-11-05 20:48:23 +0530

    Update log.js

--- src/log.js
+++ src/log.js
@@ -4,7 +4,10 @@ redux, react-redux, react-router-dom
 
 Changes in pages for
 1.Router
-Components/App.js, routes.js, index.js
+  Components/App.js, routes.js, index.js
+2. Actions/reducers
+  Components/restaurants.js, entities/restaurantsApi.js, actions/restaurants.js, reducers/restaurantsReducer.js,
+  reducers/rootReducer.js, index.js (provider, createStore)
 
 Notes-Revisit
 1. Centering nested children with flexbox
@@ -13,4 +16,9 @@ Notes-Revisit
   https://codeburst.io/getting-started-with-react-router-5c978f70df91
 3. Provider makes redux store available to nested components wrapped in connect() function
   https://react-redux.js.org/api/provider
+4. Dispatch actions with redux
+  //Maintain data from api in redux state and others in local state
+  // Need for thunk, applymiddleware, configurestore??
+  // check how mapstatetoprops takes reducer state
+  https://redux.js.org/tutorials/fundamentals/part-2-concepts-data-flow
 */
