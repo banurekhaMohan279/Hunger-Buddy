be82d19a3f9ec3d2a816a81447882400938f2907
banurekhaMohan279 <banurekha279@gmail.com>
2020-11-09 14:02:27 +0530

    Route Logo click to Home

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,5 +1,14 @@
+import {useHistory} from 'react-router-dom';
+
 export default function AppHeader (){
+
+  let history = useHistory();
+  
+  function gotoHome(){
+    history.push('/Location');
+  }
+
   return (
-    <header className = "AppHeader"> Food Delivery App </header>
+    <header className = "AppHeader" onClick = {gotoHome}> Food Delivery App </header>
   )
 }
