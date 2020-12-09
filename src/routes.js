1bf95e17cad256a301da545abdd6f771981b325b
banurekhaMohan279 <banurekha279@gmail.com>
2020-12-09 18:48:59 +0530

    Lazy loading in components

--- src/routes.js
+++ src/routes.js
@@ -1,28 +1,31 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import App from './components/App';
 import Location from './components/location.js';
-import Restaurants from './components/restaurants.js';
-import RestaurantDetails from './components/restaurantDetails.js';
+//import Restaurants from './components/restaurants.js';
+//import RestaurantDetails from './components/restaurantDetails.js';
+const Restaurants = lazy(() => import ('./components/restaurants.js'));
+const RestaurantDetails = lazy(() => import ('./components/restaurantDetails.js'));
 
 export default function Routes() {
   return(
       <Router>
-        <Switch>
-          <Route exact path = '/Hunger-Buddy'>
-            <App />
-          </Route>
-          <Route path = '/Location'>
-            <Location />
-          </Route>
-          <Route path = '/Restaurants'>
-            <Restaurants />
-          </Route>
-          <Route path = '/RestaurantDetails'>
-            <RestaurantDetails />
-          </Route>
-        </Switch>
+        <Suspense fallback = {<div> Loading... </div>}>
+            <Switch>
+              <Route exact path = '/Hunger-Buddy'>
+                <App />
+              </Route>
+              <Route path = '/Location'>
+                <Location />
+              </Route>
+              <Route path = '/Restaurants' component = {Restaurants} >
+              </Route>
+              <Route path = '/RestaurantDetails'>
+                <RestaurantDetails />
+              </Route>
+            </Switch>
+        </Suspense>
       </Router>
   )
 }
