8123d39b3d8792a1753ca16a70012541f76010de
banurekhaMohan279 <banurekha279@gmail.com>
2021-01-07 13:44:48 +0530

    Update Restaurants page in React Hooks

--- src/components/restaurants.js
+++ src/components/restaurants.js
@@ -1,30 +1,28 @@
-import React from 'react';
-import {connect} from 'react-redux';
+import React, {useEffect, useState} from 'react';
+import {connect, useDispatch, useSelector} from 'react-redux';
 import {getAllRestaurants} from '../actions/restaurants';
 import {getRestaurantDetails} from '../actions/restaurantDetails';
 import AppHeader from './AppHeader';
-import {withRouter} from 'react-router-dom';
+import {useHistory} from 'react-router-dom';
 
-class Restaurants extends React.Component{
-  constructor(props){
-    super(props);
-    this.getRestaurantDetails = this.getRestaurantDetails.bind(this);
-  }
+function Restaurants() {
+  let dispatch = useDispatch();
+  let history = useHistory();
+  let restaurants = useSelector(state => state.restaurantsReducer.restaurants && state.restaurantsReducer.restaurants.restaurants);
+  let city = useSelector(state => state.getCitiesReducer.cities["location_suggestions"][0]["entity_id"]);
+  console.log("restaurants",restaurants);
 
-  componentDidMount(){
-    let city = this.props.cities["location_suggestions"][0]["entity_id"];
-    this.props.dispatch(getAllRestaurants(city));
-  }
+  useEffect(() =>{
+    dispatch(getAllRestaurants(city));
+  },[]);
 
-  getRestaurantDetails(res_id){
-      this.props.dispatch(getRestaurantDetails(res_id)).then (() => {
-        this.props.history.push('/RestaurantDetails');
+  function getRestaurantDetails(res_id){
+      dispatch(getRestaurantDetails(res_id)).then (() => {
+        history.push('/RestaurantDetails');
       });
   }
 
-  render(){
-    let restaurants = this.props.restaurants && this.props.restaurants.restaurants;
-    return(
+  return(
       <div className = "retaurantsPage">
           <AppHeader/>
           <div className = "restaurantList">
@@ -42,17 +40,7 @@ class Restaurants extends React.Component{
             }
           </div>
       </div>
-
     )
-  }
-
 }
 
-function mapStateToProps(state){
-  return{
-    restaurants : state.restaurantsReducer.restaurants,
-    cities: state.getCitiesReducer.cities
-  }
-}
-
-export default withRouter(connect(mapStateToProps)(Restaurants));
+export default Restaurants;
