bc18fcb7e36926607ed5949f322466253afa3925
banurekhaMohan279 <banurekha279@gmail.com>
2021-01-12 17:20:33 +0530

    Style input fields in login page

--- src/components/restaurants.js
+++ src/components/restaurants.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import {connect, useDispatch, useSelector} from 'react-redux';
 import {getAllRestaurants} from '../actions/restaurants';
 import {getRestaurantDetails} from '../actions/restaurantDetails';
@@ -34,6 +34,47 @@ function Restaurants() {
       });
   }
 
+  const [postList, setPostList] = useState({
+        list: [1,2,3,4]
+    });
+    // tracking on which page we currently are
+    const [page, setPage] = useState(1);
+    // add loader refrence
+    const loader = useRef(null);
+
+    useEffect(() => {
+         var options = {
+            root: null,
+            rootMargin: "20px",
+            threshold: 1.0
+         };
+        // initialize IntersectionObserver
+        // and attaching to Load More div
+         const observer = new IntersectionObserver(handleObserver, options);
+         if (loader.current) {
+            observer.observe(loader.current)
+         }
+
+    }, []);
+
+
+    useEffect(() => {
+        // here we simulate adding new posts to List
+        const newList = postList.list.concat([1,1,1,1]);
+        setPostList({
+            list: newList
+        })
+    }, [page])
+
+    // here we handle what happens when user scrolls to Load More div
+   // in this case we just update page variable
+    const handleObserver = (entities) => {
+        const target = entities[0];
+        if (target.isIntersecting) {
+            setTimeout (() => setPage((page) => page + 1),2000);
+        }
+    }
+
   return(
       <div className = "retaurantsPage">
           <AppHeader/>
@@ -50,6 +91,9 @@ function Restaurants() {
                 );
               })
             }
+          </div>
+          <div className="loading" ref={loader}>
+                    <h2>Load More</h2>
           </div>
       </div>
     )
--- src/entities/restaurantsApi.js
+++ src/entities/restaurantsApi.js
@@ -1,7 +1,7 @@
 class restaurantsApi {
 
    getAllRestaurants(city){
-    return fetch('https://developers.zomato.com/api/v2.1/search?entity_id='+city+'&entity_type=city&category=6',
+    return fetch('https://developers.zomato.com/api/v2.1/search?entity_id='+city+'&entity_type=city&count=100&category=6',
                 {
                   method: 'GET',
                   headers: {"user-key" : "4b22cef4949b6dd368a5b9a32cb83fbd"}
