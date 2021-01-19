500352f9dac264b64a3333078d39e2d3e068637e
banurekhaMohan279 <banurekha279@gmail.com>
2021-01-19 12:26:45 +0530

    Update Login page with bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,9 @@
 import {useHistory} from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   console.log("in appp..")
@@ -10,18 +15,31 @@ function App() {
   }
 
   return (
-    <div className="App LoginPage">
-      <section className="loginContainer">
-        <div className="login">
-          <header>Food Delivery App Login</header>
-          <form onSubmit = {goToHomePage}>
-              <input type="text" placeholder="User Name"/>
-              <input type="password" placeholder="Password"/>
-              <input type="submit"/>
-          </form>
-          </div>
-      </section>
-    </div>
+      <Container fluid className = "set-height">
+        <Row className = "justify-content-md-center align-items-center set-height">
+          <Row className="justify-content-md-center login-form">
+            <Col xl>
+              <header>Food Delivery App Login</header>
+              <Form onSubmit = {goToHomePage}>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" placeholder="Enter email" />
+                  <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                  </Form.Text>
+                </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                  Submit
+                </Button>
+              </Form>
+            </Col>
+          </Row>
+        </Row>
+      </Container>
   );
 }
 
